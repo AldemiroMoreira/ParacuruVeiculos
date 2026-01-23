d46b473b4384435ed4d0f83b1585b5a22155d48f
Aldemiro Moreira <aldemiro@paracuru.local>
2026-01-23 00:48:47 -0300

    Fix: Aumentar timeout de conexao com banco de dados para 10s

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,16 +7,16 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-        host: process.env.DB_HOST,
-        dialect: 'mariadb',
-        logging: false, // Set to console.log to see SQL queries
         port: process.env.DB_PORT || 3306,
         dialectOptions: process.env.DB_SSL === 'true' ? {
             ssl: {
                 require: true,
                 rejectUnauthorized: false
-            }
-        } : {}
+            },
+            connectTimeout: 10000 // 10 seconds timeout
+        } : {
+            connectTimeout: 10000
+        }
     }
 );
 
